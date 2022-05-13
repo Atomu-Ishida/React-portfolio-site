825950a9227a20a058d4a412a11775cb4e214ec8
Atomu-Ishida <atomu.10.18@icloud.com>
2022-05-13 19:59:53 +0900

    Add 3D voxel dog

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,13 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
 import Navbar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
+import VoxelDogLoader from '../voxel-dog-loader'
+
+const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
+  ssr: false,
+  loading: () => <VoxelDogLoader />
+})
 
 const Main = ({ children, router }) => {
   return (
@@ -16,6 +23,8 @@ const Main = ({ children, router }) => {
       <Navbar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
+        <LazyVoxelDog />
+
         {children}
       </Container>
     </Box>
