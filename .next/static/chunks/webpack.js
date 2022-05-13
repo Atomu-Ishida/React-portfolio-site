1501d2ad38942686a35c6503f8e47c9ed6f51bc9
Atomu-Ishida <atomu.10.18@icloud.com>
2022-05-13 13:58:17 +0900

    Add my bio

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c12bcf22b5d06674"; }
+/******/ 		__webpack_require__.h = function() { return "532d54cc9f4af2ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
