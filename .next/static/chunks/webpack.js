b06ec1b82965f08e34f98db16fd2276e5fb9ecb2
Atomu-Ishida <atomu.10.18@icloud.com>
2022-05-15 16:48:14 +0900

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "92d68e3be477d0ae"; }
+/******/ 		__webpack_require__.h = function() { return "21fc7b3004c88f41"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
-import Navbar from '../navbar'
+import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
+import Footer from '../footer'
 import VoxelDogLoader from '../voxel-dog-loader'
 
 const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
@@ -13,19 +14,32 @@ const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        ></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Atomu's homepage" />
+        <meta name="author" content="Atomu Ishida" />
+        <meta name="author" content="craftzdog" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@craftzdog" />
+        <meta name="twitter:creator" content="@craftzdog" />
+        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:site_name" content="Atomu Ishida" />
+        <meta name="og:title" content="Atomu Ishida" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://www.craftz.dog/card.png" />
         <title>Atomu Ishida - Homepage</title>
       </Head>
 
-      <Navbar path={router.asPath} />
+      <NavBar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
         <LazyVoxelDog />
 
         {children}
+
+        <Footer />
       </Container>
     </Box>
   )
