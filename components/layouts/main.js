9a57633e3e8805a7d90d374f22b05303864a6933
Atomu-Ishida <atomu.10.18@icloud.com>
2022-05-15 18:21:07 +0900

    headを修正

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -16,14 +16,8 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Atomu's homepage" />
         <meta name="author" content="Atomu Ishida" />
-        <meta name="author" content="craftzdog" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon1.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
         <meta property="og:site_name" content="Atomu Ishida" />
         <meta name="og:title" content="Atomu Ishida" />
         <meta property="og:type" content="website" />
